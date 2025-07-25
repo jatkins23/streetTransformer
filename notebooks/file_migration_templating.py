3a1ac4ec311589c2fa36dcc423c9f02865166376
Jon Atkins <jatkins23@users.noreply.github.com>
2025-07-25 17:10:52 -0400

    update notebooks

--- notebooks/file_migration_templating.py
+++ notebooks/file_migration_templating.py
@@ -4,5 +4,8 @@ f"cp -r ../proj_data/tile2net_export/dt_bk/{year}/tiles/dt_bk_{year}_256_info.js
 f"cp -r ../proj_data/tile2net_export/dt_bk/{year}/tiles/dt_bk_2004_256_info.json data/test_runs/downtown_bk/imagery/raw_static/z19/2004/2004_256_info.json"
 
 f"cp -r ../proj_data/tile2net_export/dt_bk/{year}/tiles/static/nyc/256_{z_level} data/test_runs/downtown_bk/imagery/raw_static/z{z_level}/{year}"
+for year in range(2016, 2024, step=2):
 f"cp -r ../proj_data/tile2net_export/dt_bk/{year}/tiles/{original_name}_256_info.json data/test_runs/downtown_bk/imagery/raw_static/z{z_level}/{year}/{year}_256_info.json"
 f"cp -r ../proj_data/tile2net_export/dt_bk/{year}/tiles/{original_name}_256_info.json data/test_runs/downtown_bk/imagery/raw_static/z{z_level}/{year}}/{year}_256_info.json"
+
+
