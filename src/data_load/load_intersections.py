a3546d3ea5dcd5cc2f2173cb5d2ddb4f20aaaf0d
Jon Atkins <jatkins23@users.noreply.github.com>
2025-07-25 11:44:55 -0400

    rewrite to work with LION intersection dataset instead of ox

--- src/data_load/load_intersections.py
+++ src/data_load/load_intersections.py
@@ -8,7 +8,8 @@ load_dotenv()
 sys.path.append(str(os.getenv('INTX_PROFILING_SRC_PATH')))
 from intersection import Intersections
 
-def load_location(place):
+def load_location(place, silent=False):
+
     inter = Intersections.from_place(place)
     inter.with_options(tolerance=20)
 
---
+++ src/data_load/load_lion.py
@@ -0,0 +1,30 @@
+import os
+from pathlib import Path
+import geopandas as gpd
+
+from dotenv import load_dotenv
+load_dotenv()
+
+DATA_PATH = Path(str(os.getenv('DATA_PATH')))
+
+root_path = Path(__file__).resolve().parent.parent.parent
+LION_PATH = 'data/lion'
+
+lion_nodes = gpd.read_file(root_path / LION_PATH / 'lion.gdb', layer='node')
+lion_node_names = gpd.read_file(root_path / LION_PATH / 'lion.gdb', layer='node_stname')
+lion_altnames = gpd.read_file(root_path / LION_PATH / 'lion.gdb', layer='altnames') # Alternate names for streets (e.g. Ave of Americas)
+lion_lion = gpd.read_file(root_path / LION_PATH / 'lion.gdb', layer='lion')
+
+# lion_nodes.explore()
+
+all_intersections = lion_nodes.merge(
+    lion_node_names
+        .groupby('NodeId')['StreetName']
+        .apply(' & '.join)
+        .reset_index(),
+    how='left',
+    left_on = 'NODEID', right_on = 'NodeId',
+    indicator=True
+)[['StreetName','NODEID','geometry', 'GLOBALID']].set_index('NODEID')
+
+all_intersections.to_csv(root_path / DATA_PATH.parent / 'nyc' / 'lion_intersections.csv', index=False)
